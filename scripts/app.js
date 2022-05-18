a18a2063a04a034cea2ddf184fc647dff41cc519
octavio <octaviosuriellopezlopez@gmail.com>
2022-05-17 23:43:45 -0500

    consultar saldo implementado completamente

--- scripts/app.js
+++ scripts/app.js
@@ -3,45 +3,67 @@ const parametros = window.location.search;
 //Creamos la instancia
 const urlParams = new URLSearchParams(parametros);
 //Accedemos a el id
-let id = urlParams.get('id');
+let id = urlParams.get("id");
 
 //NaV
-let nombreCompleto=document.getElementById('nombreCompleto');
+let nombreCompleto = document.getElementById("nombreCompleto");
+//Cuerpo
+let boxInfoAcciones=document.getElementById('info_acciones');
 
-let datosCuenta={
-    idCuenta:'',
-    nombre:'',
-    usuario:'',
-    password:'',
-    saldo:0
+//Objeto para almacenar valores de mi cuenta
+let datosCuenta = {
+  idCuenta: "",
+  nombre: "",
+  usuario: "",
+  password: "",
+  saldo: 0,
 };
 
-document.addEventListener('DOMContentLoaded',()=>{
-    cuentas.forEach(cuenta => {
-        if(localStorage.getItem("cuenta_"+id)) datosCuenta=JSON.parse(localStorage.getItem('cuenta_'+id));
-        else if(cuenta.idCuenta==id)datosCuenta=cuenta;
+document.addEventListener("DOMContentLoaded", () => {
+  cuentas.forEach((cuenta) => {
+    if (localStorage.getItem("cuenta_" + id))
+      datosCuenta = JSON.parse(localStorage.getItem("cuenta_" + id));
+    else if (cuenta.idCuenta == id) datosCuenta = cuenta;
 
-        nombreCompleto.textContent=datosCuenta.nombre;
-    });
+    nombreCompleto.textContent = datosCuenta.nombre;
+  });
 });
 
-
+//Funciones para consultar saldo
 function consultaSaldo(){
-    alert("Su saldo es de: "+datosCuenta.saldo);
+    const divSaldo=document.createElement('div');
+    var btnOK=document.createElement('button');
+    divSaldo.classList.add('bg-black','text-white','border-danger','p-4','text-center','w-100','bg-opacity-75');
+    divSaldo.textContent="Su saldo es de: "+datosCuenta.saldo;
+    divSaldo.style.fontSize=25+"px";
+    btnOK.textContent="OK";
+    btnOK.classList.add('bg-success','text-white','text-center','border-success','rounded-3','mx-2');
+    divSaldo.appendChild(btnOK);
+    boxInfoAcciones.appendChild(divSaldo);
+    //Cierra el Div Al presionar el boton
+    btnOK.onclick=function(){
+        if(btnOK.onclick){
+            while(boxInfoAcciones.firstChild){
+                boxInfoAcciones.firstChild.remove();
+            }
+        }
+    }
 }
 
-function ingresar(){
-    let salgoIngresado=Number(prompt("Ingreso:"));
-    let nuevoSaldo=Number(datosCuenta.saldo+salgoIngresado);
-    if(nuevoSaldo>990){
-        alert("No puede tener mas de 990 pesos");
-    }else{
-    cuentas.forEach(cuenta => {
-        if(cuenta.idCuenta==datosCuenta.idCuenta)
-        {   
-            cuenta.saldo=datosCuenta.saldo;
-            localStorage.setItem("cuenta_"+cuenta.idCuenta,JSON.stringify(cuenta));
-        }
+function ingresar() {
+  let salgoIngresado = Number(prompt("Ingreso:"));
+  let nuevoSaldo = Number(datosCuenta.saldo + salgoIngresado);
+  if (nuevoSaldo > 990) {
+    alert("No puede tener mas de 990 pesos");
+  } else {
+    cuentas.forEach((cuenta) => {
+      if (cuenta.idCuenta == datosCuenta.idCuenta) {
+        cuenta.saldo = datosCuenta.saldo;
+        localStorage.setItem(
+          "cuenta_" + cuenta.idCuenta,
+          JSON.stringify(cuenta)
+        );
+      }
     });
-}
+  }
 }
