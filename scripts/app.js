7517d844094a597fb566bc56f238f822f05c81a0
octavio <octaviosuriellopezlopez@gmail.com>
2022-05-18 11:29:23 -0500

    se implemento a un 95% la parte de ingresar monto

--- scripts/app.js
+++ scripts/app.js
@@ -29,41 +29,108 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+//BORRAR TODO DE DIV INFO
+function borrarElementosInfo(){
+  while(boxInfoAcciones.firstChild){
+    boxInfoAcciones.firstChild.remove();
+  }
+}
+
 //Funciones para consultar saldo
 function consultaSaldo(){
     const divSaldo=document.createElement('div');
     var btnOK=document.createElement('button');
-    divSaldo.classList.add('bg-black','text-white','border-danger','p-4','text-center','w-100','bg-opacity-75');
-    divSaldo.textContent="Su saldo es de: "+datosCuenta.saldo;
+    divSaldo.classList.add('bg-black','text-white','border-danger','p-4','text-center','mx-auto','col-xl-3','col-10','col-sm-6','col-md-5','col-lg-3','bg-opacity-75');
+    divSaldo.textContent="Su saldo es de: $"+datosCuenta.saldo;
     divSaldo.style.fontSize=25+"px";
-    btnOK.textContent="OK";
+    btnOK.textContent="OK!";
     btnOK.classList.add('bg-success','text-white','text-center','border-success','rounded-3','mx-2');
     divSaldo.appendChild(btnOK);
     boxInfoAcciones.appendChild(divSaldo);
     //Cierra el Div Al presionar el boton
     btnOK.onclick=function(){
         if(btnOK.onclick){
-            while(boxInfoAcciones.firstChild){
-                boxInfoAcciones.firstChild.remove();
-            }
+           borrarElementosInfo();
         }
     }
 }
 
-function ingresar() {
-  let salgoIngresado = Number(prompt("Ingreso:"));
-  let nuevoSaldo = Number(datosCuenta.saldo + salgoIngresado);
-  if (nuevoSaldo > 990) {
+
+//FUNCIONES PARA INGREGAR MONTO
+function crearDivIngresar(){
+  const divIngreso=document.createElement('div');
+  divIngreso.classList.add('bg-black','text-white','p-4','text-center','bg-opacity-75','mx-auto','col-xl-3','col-6','col-sm-6','col-md-5','col-lg-4');
+  divIngreso.textContent="Monto a ingresar:";
+  divIngreso.style.fontSize=25+"px";
+  var inputMonto=document.createElement('input');
+  inputMonto.classList.add('mx-2','rounded-3');
+  inputMonto.style.width=100+"px";
+  inputMonto.type='Number';
+  inputMonto.placeholder='0.00'
+  var btnIngresar=document.createElement('button');
+  btnIngresar.classList.add('bg-success','text-white','text-center','border-success','rounded-3','my-3','bi','bi-check-lg');
+  var btnCancelar=document.createElement('button');
+  btnCancelar.classList.add('bg-danger','text-white','text-center','border-danger','rounded-3','my-3','ms-2','bi', 'bi-x');
+  divIngreso.appendChild(inputMonto);
+  divIngreso.appendChild(btnIngresar);
+  divIngreso.appendChild(btnCancelar);
+  boxInfoAcciones.appendChild(divIngreso);
+  btnIngresar.onclick=function(){
+      ingresar(Number(inputMonto.value));
+  }
+  btnCancelar.onclick=function(){
+    borrarElementosInfo();
+}
+}
+
+function ingresar(ingreso) {
+  let saldoNuevo=datosCuenta.saldo+ingreso;
+  if (saldoNuevo > 990) {
     alert("No puede tener mas de 990 pesos");
   } else {
     cuentas.forEach((cuenta) => {
       if (cuenta.idCuenta == datosCuenta.idCuenta) {
-        cuenta.saldo = datosCuenta.saldo;
-        localStorage.setItem(
+          cuenta.saldo = saldoNuevo;
+          datosCuenta.saldo=saldoNuevo;
+          localStorage.setItem(
           "cuenta_" + cuenta.idCuenta,
           JSON.stringify(cuenta)
         );
+            
+            borrarElementosInfo();
+            mostrarNSaldo(ingreso,saldoNuevo,1);
       }
     });
   }
 }
+
+function mostrarNSaldo(ingreso,nSaldo,tipo){
+    const divNSaldo=document.createElement('div');
+    const pMenMonto=document.createElement('p');
+    const pMonto=document.createElement('span');
+    const pNSaldo=document.createElement('p');
+    const btnOK=document.createElement('button');
+    divNSaldo.classList.add('bg-black','text-white','border-danger','p-4','text-center','mx-auto','col-xl-3','col-10','col-sm-6','col-md-5','col-lg-4','bg-opacity-75');
+    if(tipo==1){
+      pMenMonto.textContent="El Monto Ingresado Fue De: ";
+      pMonto.textContent="$"+ingreso;
+      pMonto.classList.add('text-success');
+    }
+    pMenMonto.style.fontSize=20+"px";
+    pMonto.style.fontSize=25+"px";
+    pNSaldo.style.fontSize=20+"px";
+    pNSaldo.textContent="El nuevo Saldo Es De: $"+nSaldo;
+    btnOK.textContent="OK!";
+    btnOK.classList.add('bg-success','text-white','text-center','border-success','rounded-3','mx-2');
+    divNSaldo.appendChild(pMenMonto);
+    divNSaldo.appendChild(pMonto);
+    divNSaldo.appendChild(pNSaldo);
+    divNSaldo.appendChild(btnOK);
+    boxInfoAcciones.appendChild(divNSaldo);
+    btnOK.onclick=function(){
+      if(btnOK.onclick){
+         borrarElementosInfo();
+      }
+  }
+}
+
