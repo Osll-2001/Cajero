3a5231134f6428140a9ec9bdc5d65bda15b7b435
octavio <octaviosuriellopezlopez@gmail.com>
2022-05-19 17:06:18 -0500

    posible solucion teclado

--- scripts/app.js
+++ scripts/app.js
@@ -151,7 +151,7 @@ function noNegativos(input){
   input.onkeydown = function(e) {
     if(!((e.keyCode > 95 && e.keyCode < 106)
       || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8 || e.keyCode==190)) {
+      || e.keyCode == 8 || e.keyCode==190 || e.keyCode==69)) {
         return false;
     }
 }
