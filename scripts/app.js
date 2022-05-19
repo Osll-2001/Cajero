1114f45c8452c5eab01f3b7e094789fa926772e3
octavio <octaviosuriellopezlopez@gmail.com>
2022-05-18 20:15:47 -0500

    se termino de implementar la funcionalidad de ingreso y retiro con unas posibles mejoras en diseño

--- scripts/app.js
+++ scripts/app.js
@@ -60,17 +60,21 @@ function noNegativos(input){
   input.onkeydown = function(e) {
     if(!((e.keyCode > 95 && e.keyCode < 106)
       || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8)) {
+      || e.keyCode == 8 || e.keyCode==190)) {
         return false;
     }
 }
 }
 
-//FUNCIONES PARA INGREGAR MONTO
-function crearDivIngresar(){
+//FUNCIONES PARA INGREGAR Y RETIRAR MONTO
+
+//CREA UN DIV PARA INGRESAR EL MONTO A RETIRAR O INGRESAR
+function crearDivAccion(tipo){
   const divIngreso=document.createElement('div');
   divIngreso.classList.add('bg-black','text-white','p-4','text-center','bg-opacity-75','mx-auto','col-xl-3','col-6','col-sm-6','col-md-5','col-lg-4');
-  divIngreso.textContent="Monto a ingresar:";
+  //CAMBIA EL TEXTO DEPENDIENDO DE EL TIPO DE ACCION
+  if(tipo==1)divIngreso.textContent="Monto a ingresar:";
+  else divIngreso.textContent="Monto a retirar: ";
   divIngreso.style.fontSize=25+"px";
   var inputMonto=document.createElement('input');
   inputMonto.classList.add('mx-2','rounded-3');
@@ -88,45 +92,79 @@ function crearDivIngresar(){
   divIngreso.appendChild(btnCancelar);
   boxInfoAcciones.appendChild(divIngreso);
   btnIngresar.onclick=function(){
-      ingresar(Number(inputMonto.value));
+      if(tipo==1) ingresar(Number(inputMonto.value));
+      else if(tipo==2) retirar(Number(inputMonto.value));
   }
   btnCancelar.onclick=function(){
     borrarElementosInfo();
 }
 }
 
+//MODIFICA EL SALDO
+function cambioSaldo(idCuenta,saldoNuevo){
+  cuentas.forEach((cuenta) => {
+    if (cuenta.idCuenta == idCuenta) {
+        cuenta.saldo = saldoNuevo;
+        datosCuenta.saldo=saldoNuevo;
+        localStorage.setItem(
+        "cuenta_" + cuenta.idCuenta,
+        JSON.stringify(cuenta)
+      );
+    }
+  });
+}
+
+//MENSAJE DE ADVERTENCIA
+function menAdvertencia(mensaje){
+  const divMenAdvertencia=document.createElement('div');
+  divMenAdvertencia.classList.add('bg-danger','text-white','border-danger','p-2','text-center','col-xl-3','col-6','col-sm-6','col-md-5','col-lg-4','mx-auto');
+  divMenAdvertencia.textContent=mensaje;
+  boxInfoAcciones.appendChild(divMenAdvertencia);
+}
+
+//FUNCION PARA HACER EL PROCESO DE INGRESO A EL SALDO
 function ingresar(ingreso) {
   let saldoNuevo=datosCuenta.saldo+ingreso;
   if (saldoNuevo > 990) {
-    alert("No puede tener mas de 990 pesos");
+    menAdvertencia("No se puede tener mas de $990 en esta cuenta!");
   } else {
-    cuentas.forEach((cuenta) => {
-      if (cuenta.idCuenta == datosCuenta.idCuenta) {
-          cuenta.saldo = saldoNuevo;
-          datosCuenta.saldo=saldoNuevo;
-          localStorage.setItem(
-          "cuenta_" + cuenta.idCuenta,
-          JSON.stringify(cuenta)
-        );
-            
-            borrarElementosInfo();
-            mostrarNSaldo(ingreso,saldoNuevo,1);
-      }
-    });
+      cambioSaldo(datosCuenta.idCuenta,saldoNuevo);
+      borrarElementosInfo();
+      mostrarNSaldo(ingreso,saldoNuevo,1);
   }
 }
 
-function mostrarNSaldo(ingreso,nSaldo,tipo){
+//FUNCION PARA RETIRAR DINERO
+function retirar(retiro){
+  let saldoNuevo=datosCuenta.saldo-retiro;
+  if(saldoNuevo<10){
+    menAdvertencia("No se puede tener menos de $10 en esta cuenta!");
+  }
+  else{
+      cambioSaldo(datosCuenta.idCuenta,saldoNuevo);
+      borrarElementosInfo();
+      mostrarNSaldo(retiro,saldoNuevo,2);
+  }
+}
+
+//MUESTRA EL MONTO INGRESADO O RETIRADO Y EL SALDO ACTUAL
+function mostrarNSaldo(cantidad,nSaldo,tipo){
     const divNSaldo=document.createElement('div');
     const pMenMonto=document.createElement('p');
     const pMonto=document.createElement('span');
     const pNSaldo=document.createElement('p');
     const btnOK=document.createElement('button');
     divNSaldo.classList.add('bg-black','text-white','border-danger','p-4','text-center','mx-auto','col-xl-3','col-10','col-sm-6','col-md-5','col-lg-4','bg-opacity-75');
+    //DEPENDIENDO DE EL TIPO CAMBIA EL TEXTO
     if(tipo==1){
       pMenMonto.textContent="El Monto Ingresado Fue De: ";
-      pMonto.textContent="$"+ingreso;
+      pMonto.textContent="$"+cantidad;
       pMonto.classList.add('text-success');
+    }
+    else if(tipo==2){
+      pMenMonto.textContent="El Monto Retirado Fue De: ";
+      pMonto.textContent="$"+cantidad;
+      pMonto.classList.add('text-danger');
     }
     pMenMonto.style.fontSize=20+"px";
     pMonto.style.fontSize=25+"px";
