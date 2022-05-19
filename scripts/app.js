02b5cf4e8ed1fc9d1fcc572741fde7add5dfeed9
octavio <octaviosuriellopezlopez@gmail.com>
2022-05-19 17:11:41 -0500

    prueba teclado 2

--- scripts/app.js
+++ scripts/app.js
@@ -152,7 +152,7 @@ function noNegativos(input){
     alert(e.keyCode);
     if(!((e.keyCode > 95 && e.keyCode < 106)
       || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8 || e.keyCode==190 || e.keyCode==69)) {
+      || e.keyCode == 8 || e.keyCode==190 || e.keyCode==109)) {
         return false;
     }
 }
